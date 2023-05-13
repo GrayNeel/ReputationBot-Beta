1025e88dcc22025a3ad544f6aa4193043b882fe2
GrayNeel <paoul32@yahoo.it>
2023-05-13 13:17:05 +0200

    first commit

---
+++ bot_home.js
@@ -0,0 +1,10 @@
+const { Bot } = require("grammy");
+
+// Create a bot object
+const bot = new Bot(""); // <-- place your bot token in this string
+
+// Register listeners to handle messages
+bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
+
+// Start the bot (using long polling)
+bot.start();
