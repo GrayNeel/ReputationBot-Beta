b6aca42803f4b12ae7cd390afdbf2dcfa8d840ef
GrayNeel <paoul32@yahoo.it>
2023-05-13 13:21:39 +0200

    feat: added bot private key

--- bot_home.js
+++ bot_home.js
@@ -1,7 +1,8 @@
 const { Bot } = require("grammy");
+const privateKey = require('./private');
 
 // Create a bot object
-const bot = new Bot(""); // <-- place your bot token in this string
+const bot = new Bot(privateKey); // <-- place your bot token in this string
 
 // Register listeners to handle messages
 bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
