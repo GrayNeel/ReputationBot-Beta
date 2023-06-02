95f4428603c0a582f6d35f3398cae27b15800a9c
Alessio Bincoletto <s234970@studenti.polito.it>
2023-06-02 17:24:08 +0200

    Script correctly parses all entries in the dump

--- old-db/extract_groups.py
+++ old-db/extract_groups.py
@@ -6,11 +6,14 @@ output_file = "output.sql"
 
 # Open the input file
 with open(input_file, "r") as file:
-    # Initialize flag and group_lines list
+    # Initialize flag and group_lines dictionary
     processing_groups = False
-    group_lines = []
+    group_lines = {}
+
+    # Initialize flag and user_lines dictionary
     processing_users = False
-    user_lines = []
+    user_lines = {}
+    
     user_in_group_lines = []
 
     # Read the file line by line
@@ -27,20 +30,28 @@ with open(input_file, "r") as file:
 
             # Check if the line matches the expected format
             if line.startswith("(") and line.endswith("),"):
-                # Remove parentheses and split the line by commas
-                values = line[1:-2].split(",")
+                # Remove parentheses and split the line by tick + comma + space to isolate string fields
+                split_on_strings_values = line[1:-2].split("', ")
+                # divide chat id from the title
+                id_and_title = split_on_strings_values[0].split(", ")
 
-                # Extract the values
-                chatid = values[0].strip()
-                title = values[1].strip().strip("'")
-                group_type = values[2].strip().strip("'")
-                is_silent = values[3].strip()
+                # Extract the chatid
+                chatid = id_and_title[0].strip()
+                # compose the title string by joining the values from id_and_title[1:]
+                preprocessed_title = ", ".join(id_and_title[1:])
+                title = preprocessed_title.strip().strip("'").replace("\\'", "'").replace("'", "''")
+                # get the group type
+                group_type = split_on_strings_values[1].strip().strip("'")
+                if split_on_strings_values[2].strip() == "1": # is_silent
+                    is_silent = "true"
+                else:
+                    is_silent = "false"
 
                 # Create the INSERT INTO statement
                 insert_statement = f"INSERT INTO \"Group\" (\"chatid\", \"name\", \"type\", \"is_silent\") VALUES ({chatid}, '{title}', '{group_type}', {is_silent});"
-
-                # Add the statement to the group_lines list
-                group_lines.append(insert_statement)
+                
+                # add the entry chatid: insert_statement to the group_lines dictionary
+                group_lines[chatid] = insert_statement
 
             # Break the loop if we have processed the 'groups' section
             if line.endswith(");"):
@@ -61,45 +72,62 @@ with open(input_file, "r") as file:
 
             # Check if the line matches the expected format
             if line.startswith("(") and (line.endswith("),") or line.endswith(");")) :
-                # Remove parentheses and split the line by commas
-                values = line[1:-2].split(",")
+                # Remove parentheses and split the line by tick + comma + space to isolate string fields
+                split_on_strings_values = line[1:-2].split("', '")
+                # Remove parentheses and split the line by commas+space
+                usr_chat_firstname = split_on_strings_values[0].split(", ")
 
                 # Extract the values
-                userid = values[0].strip()
-                chatid = values[1].strip()
-                firstname = values[2].strip().strip("'")
-                lastname = values[3].strip().strip("'")
-                username = values[4].strip().strip("'")
-                language = values[5].strip().strip("'")
-                first_seen = values[6].strip().strip("'")
-                last_seen = values[7].strip().strip("'")
-                reputation = values[8].strip()
-                reputation_today = values[9].strip()
-                messages = values[10].strip()
-                messages_today = values[11].strip()
-                up_available = values[12].strip()
-                down_available = values[13].strip()
-                beast_mode = values[14].strip()
-                cbdata = values[15].strip().strip("'")
+                userid = usr_chat_firstname[0].strip()
+                #print(userid)
+                chatid = usr_chat_firstname[1].strip()
+                # compose the firstname string by joining the values from usr_chat_firstname[2:]
+                preprocessed_firstname = ", ".join(usr_chat_firstname[2:])
+                firstname = preprocessed_firstname.strip().strip("'").replace("\\'", "'").replace("'", "''")
+
+                # process the string values
+                lastname = split_on_strings_values[1].strip().strip("'").replace("\\'", "'").replace("'", "''")
+                username = split_on_strings_values[2].strip().strip("'").replace("\\'", "'").replace("'", "''")
+                language = split_on_strings_values[3].strip().strip("'")
+                first_seen = split_on_strings_values[4].strip().strip("'")
+
+                # get the values from reputation to cbdata splitting by commas+space
+                from_lastdate_to_cbdata = split_on_strings_values[5].split(", ")
+                
+                last_seen = from_lastdate_to_cbdata[0].strip().strip("'")
+                reputation = from_lastdate_to_cbdata[1].strip()
+                reputation_today = from_lastdate_to_cbdata[2].strip()
+                messages = from_lastdate_to_cbdata[3].strip()
+                messages_today = from_lastdate_to_cbdata[4].strip()
+                up_available = from_lastdate_to_cbdata[5].strip()
+                down_available = from_lastdate_to_cbdata[6].strip()
+
+                if from_lastdate_to_cbdata[7].strip() == "1":
+                    beast_mode = "true"
+                else:
+                    beast_mode = "false"
+                
+                # compose the cbdata string by joining the values from from_lastdate_to_cbdata[7:]
+                preprocessed_cbdata = ", ".join(from_lastdate_to_cbdata[8:])
+                cbdata = preprocessed_cbdata.strip().strip("'").replace("\\'", "'").replace("'", "''")
 
                 # Create the INSERT INTO statement for the 'User' table
                 insert_statement = f"INSERT INTO \"User\" (\"userid\", \"firstname\", \"lastname\", \"username\", \"language\", \"cbdata\") VALUES ({userid}, '{firstname}', '{lastname}', '{username}', '{language}', '{cbdata}');"
+                
+                # add the entry userid: insert_statement to the user_lines dictionary
+                user_lines[userid] = insert_statement
 
-                # Add the statement to the user_lines list
-                user_lines.append(insert_statement)
+                # avoid inserting the private chats (where chatid == userid)
+                if chatid != userid:
+                    # Create the INSERT INTO statement for the 'user_in_group' table
+                    insert_statement = f"INSERT INTO \"user_in_group\" (\"id\", \"userid\", \"chatid\", \"is_admin\", \"first_seen\", \"last_seen\", \"reputation\", \"reputation_today\", \"messages\", \"messages_today\", \"up_available\", \"down_available\", \"beast_mode\") VALUES (default, {userid}, {chatid}, false, '{first_seen}', '{last_seen}', {reputation}, {reputation_today}, {messages}, {messages_today}, {up_available}, {down_available}, {beast_mode});"
 
-                # Create the INSERT INTO statement for the 'user_in_group' table
-                insert_statement = f"INSERT INTO \"user_in_group\" (\"id\", \"userid\", \"chatid\", \"is_admin\", \"first_seen\", \"last_seen\", \"reputation\", \"reputation_today\", \"messages\", \"messages_today\", \"up_available\", \"down_available\", \"beast_mode\") VALUES (NULL, {userid}, {chatid}, false, '{first_seen}', '{last_seen}', {reputation}, {reputation_today}, {messages}, {messages_today}, {up_available}, {down_available}, {beast_mode});"
+                    # Add the statement to the user_in_group_lines list
+                    user_in_group_lines.append(insert_statement)
 
-                # Add the statement to the user_in_group_lines list
-                user_in_group_lines.append(insert_statement)
-
-            # Break the loop if we have processed the 'users' section
-            #if line.endswith(");"):
-            #    break
 
 # Write the group lines to the output file
 with open(output_file, "w") as output:
-    output.writelines("\n".join(group_lines))
-    output.writelines("\n".join(user_lines))
+    output.writelines("\n".join(user_lines.values()))
+    output.writelines("\n".join(group_lines.values()))
     output.writelines("\n".join(user_in_group_lines))
