6408f377bc5134ed6a5220b3017d95ca14a944ae
Alessio Bincoletto <s234970@studenti.polito.it>
2023-06-05 03:12:34 +0200

    implement upsert and remove group functions

---
+++ bot/src/daos/group_dao.ts
@@ -0,0 +1,35 @@
+import { Group, PrismaClient} from '@prisma/client'
+const prisma = new PrismaClient();
+
+export async function upsertGroup(group:Group) {
+
+    await prisma.group.upsert({
+        where: { chatid: group.chatid },
+        update: {
+            title: group.title,
+            type: group.type,
+            is_silent: group.is_silent,
+        },
+        create: {
+            chatid: group.chatid,
+            title: group.title,
+            type: group.type,
+            is_silent: group.is_silent,
+        }
+    })
+
+}
+
+/**
+ * Must be called after the deletion of each entry in other tables that might use this groupId as a foreign key
+ * @param group 
+ */
+export async function removeGroup(group:Group) {
+
+    await prisma.group.delete({
+        where: {
+          chatid: group.chatid
+        }
+      }).catch(()=>{ throw new Error("removeGroup has failed in removing group: "+group); })
+    
+}
