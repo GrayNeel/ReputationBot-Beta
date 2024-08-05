10e2d33ab0ca29a3717c2a5edc926dd2152c7c15
Alessio Bincoletto <s234970@studenti.polito.it>
2024-08-06 00:57:08 +0200

    writers to contributors

--- bot/src/bot_home.ts
+++ bot/src/bot_home.ts
@@ -320,7 +320,7 @@ bot.command("topmess", async (ctx) => {
 
     console.log("n_users: " + n_users);
 
-    let msg = "The top " + n_users + " writers in this group are:\n";
+    let msg = "The top " + n_users + " contributors in this group are:\n";
     // get the reputation of the user in the group
     const group = group_api.parseGroup(ctx);
     // TODO: complete this function
